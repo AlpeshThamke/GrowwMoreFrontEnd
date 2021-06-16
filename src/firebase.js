4060e0eb0207e91c20ccccba30e668b47b5aaee3
AlpeshThamke <69669896+AlpeshThamke@users.noreply.github.com>
2021-06-16 08:55:09 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app"
 import 'firebase/auth'
 
 const app = firebase.initializeApp({
-    apiKey: "AIzaSyDdlKmuFVrkvFF7nG60RW-2xbjlbMSX3zM",
-    authDomain: "growwmoreapp.firebaseapp.com",
-    projectId: "growwmoreapp",
-    storageBucket: "growwmoreapp.appspot.com",
-    messagingSenderId: "668076641998",
-    appId: "1:668076641998:web:d341fc1059d783088aca9f"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 })
-
+//I have intentionally removed these values, you can add your api values provided by firebase
 export const auth = app.auth()
 export default app;
